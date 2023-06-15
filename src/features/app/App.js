879a7de8e6395ca1a2dd9e72a91176bc89b8ea58
yixellet <yixwellet@gmail.com>
2023-06-15 14:58:57 +0400

    Сделал кнопку входа в систему

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Map from '../map/Map';
 import ControlPanel from '../controlPanel/ControlPanel';
+import LoginButton from '../loginButton/LoginButton';
 
 function App() {
   return (
     <div className="App">
     <Map />
-      <ControlPanel />
+    <ControlPanel />
+    <LoginButton />
     </div>
   );
 }
